c43e088d7062b81a23ce869ec67413809616ff21
Hoglar <martinhl__@hotmail.com>
2019-11-04 15:46:54 +0100

    Ok, vet ikke om denne quotes løsningen er den beste, men vi får drive på litt

---
+++ assets/quotes.js
@@ -0,0 +1,173 @@
+'use strict';
+// Vi lager en funksjon som gir tilbake en random short quote.
+
+// Guess we only need an array?
+
+const shortQuotes = [
+    {
+        quote: "You can totally do this",
+        author: "unknown"
+    },
+    {
+        quote: "Impossible is for the unwilling",
+        author: "John Keats"
+    },
+    {
+        quote: "No pressure, no diamonds",
+        author: "Thomas Carlyle"
+    },
+    {
+        quote: "Stay foolish to stay sane",
+        author: "Maxime Lagacé"
+    },
+    {
+        quote: "When nothing goes right, go left",
+        author: "unknown"
+    },
+    {
+        quote: "Try Again. Fail again. Fail better",
+        author: "Samuel Beckett"
+    },
+    {
+        quote: "Don’t tell people your plans. Show them your results",
+        author: "unknown"
+    },
+    {
+        quote: "I can and I will",
+        author: "unknown"
+    },
+    {
+        quote: "Take the risk or lose the chance",
+        author: "unknown"
+    },
+    {
+        quote: "Prove them wrong",
+        author: "unknown"
+    },
+    {
+        quote: "And so the adventure begins",
+        author: "unknown"
+    },
+    {
+        quote: "Leave no stone unturned",
+        author: "Euripides"
+    },
+    {
+        quote: "Stay hungry. Stay foolish",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "Broken crayons still color",
+        author: "unknown"
+    },
+    {
+        quote: "If you want it, work for it",
+        author: "unknown"
+    },
+    {
+        quote: "You can if you think you can",
+        author: "George Reeves"
+    },
+    {
+        quote: "Whatever you are, be a good one",
+        author: "Abraham Lincoln"
+    },
+    {
+        quote: "Grow through what you go through",
+        author: "unknown"
+    },
+    {
+        quote: "Do it with passion or not at all",
+        author: "unknown"
+    },
+    {
+        quote: "She believed she could, so she did",
+        author: "unknown"
+    },
+    {
+        quote: "The past does not equal the future",
+        author: "Tony Robbins"
+    },
+    {
+        quote: "Good things happen to those who hustle",
+        author: "Anaïs Nin"
+    },
+    {
+        quote: "At the end of hardship comes happiness",
+        author: "unknown"
+    },
+    {
+        quote: "If it matters to you, you’ll find a way",
+        author: "Charlie Gilkey"
+    },
+    {
+        quote: "Forget about style; worry about results",
+        author: "Bobby Orr"
+    },
+    {
+        quote: "Whatever you do, do with all your might",
+        author: "Marcus Tullius Cicero"
+    },
+    {
+        quote: "Dream without fear. Love without limits",
+        author: "unknown"
+    },
+    {
+        quote: "Every noble work is at first impossible",
+        author: "Thomas Carlyle"
+    },
+    {
+        quote: "If you’re going through hell, keep going",
+        author: "Winston Churchill"
+    },
+    {
+        quote: "You can do anything you set your mind to",
+        author: "unknown"
+    },
+    {
+        quote: "We are twice armed if we fight with faith",
+        author: "Plato"
+    },
+    {
+        quote: "The wisest mind has something yet to learn",
+        author: "George Santanaya"
+    },
+    {
+        quote: "Open your mind. Get up off the couch. Move",
+        author: "Anthony Bourdain"
+    },
+    {
+        quote: "Be faithful to that which exists within yourself",
+        author: "André Gide"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+    {
+        quote: "",
+        author: "unknown"
+    },
+
+
+]
