ca77309e792005da6ce86a37ca21833f54972d76
Hoglar <martinhl__@hotmail.com>
2019-11-25 20:48:14 +0100

    er det galt med tab nav

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -29,6 +29,16 @@ const badHabitReducer = (state = [], action) => {
     if ( action.type === SAVE_NEW_BAD_HABIT ) {
         return [...state, action.payload]
     }
+    if ( action.type === COMPLETE_DAILY_BAD_HABIT ) {
+        let newHabitObject = [...state];
+        newHabitObject[action.habitIndex].points += 1000 ;
+        newHabitObject[action.habitIndex].lastUpdated = Date.now();
+        newHabitObject[action.habitIndex].weekCounter -= 1;
+
+        console.log(newHabitObject)
+        return newHabitObject;
+    }
+
     return state;
 }
 
