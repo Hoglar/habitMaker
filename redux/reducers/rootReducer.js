d0a5c39b73cb4bc7203e1672fbd1ed23ace0fdbc
Hoglar <martinhl__@hotmail.com>
2019-12-05 15:04:35 +0100

    Pynte her, pynte der!

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -13,124 +13,116 @@ import {CHANGE_QUOTE,
 
 
 const goodHabitReducer = (state = [], action) => {
-    if ( action.type === SAVE_NEW_GOOD_HABIT ) {
-        return [...state, action.payload]
-    }
-    if ( action.type === COMPLETE_DAILY_GOOD_HABIT ) {
-        let newHabitObject = [...state];
-        let today = getTodayDate();
+    switch (action.type) {
+        case SAVE_NEW_GOOD_HABIT:
+            return [...state, action.payload]
 
-        newHabitObject[action.habitIndex].points += 172800 ;
-        newHabitObject[action.habitIndex].lastUpdated = today;
-        newHabitObject[action.habitIndex].weekCounter -= 1;
+        case COMPLETE_DAILY_GOOD_HABIT: {
+            let newHabitObject = [...state];
+            let today = getTodayDate();
 
-        return newHabitObject;
-    }
+            newHabitObject[action.habitIndex].points += 172800 ;
+            newHabitObject[action.habitIndex].lastUpdated = today;
+            newHabitObject[action.habitIndex].weekCounter -= 1;
 
-    if (action.type === DECAY_HABITPOINTS_ON_START) {
-
-        let newHabitObject = [...state];
-        for (i = 0; i < newHabitObject.length; i++) {
-            let modifiedDecayPoints = Math.floor(action.decayPoints * newHabitObject[i].difficulity)
-            if ((newHabitObject[i].points - modifiedDecayPoints) <= 0 ) {
-                newHabitObject[i].points = 0;
-            } else {
-
-                newHabitObject[i].points -= modifiedDecayPoints;
-            }
+            return newHabitObject;
         }
-        return newHabitObject;
 
-    }
-
-    if (action.type === RESET_WEEKLY_COUNTER) {
-        let newHabitObject = [...state];
-        if(newHabitObject.length > 0) {
+        case DECAY_HABITPOINTS_ON_START: {
+            let newHabitObject = [...state];
             for (i = 0; i < newHabitObject.length; i++) {
-                newHabitObject[i].weekCounter = newHabitObject[i].weekCounterLimit;
+                let modifiedDecayPoints = Math.floor(action.decayPoints * newHabitObject[i].difficulity)
+                if ((newHabitObject[i].points - modifiedDecayPoints) <= 0 ) {
+                    newHabitObject[i].points = 0;
+                } else {
+
+                    newHabitObject[i].points -= modifiedDecayPoints;
+                }
             }
+            return newHabitObject;
         }
+        case RESET_WEEKLY_COUNTER: {
+            let newHabitObject = [...state];
+            if(newHabitObject.length > 0) {
+                for (i = 0; i < newHabitObject.length; i++) {
+                    newHabitObject[i].weekCounter = newHabitObject[i].weekCounterLimit;
+                }
+            }
 
-        return newHabitObject;
-
+            return newHabitObject;
+        }
+        default:
+            return state;
     }
-
-    return state;
 }
 
 const badHabitReducer = (state = [], action) => {
-    if ( action.type === SAVE_NEW_BAD_HABIT ) {
-        return [...state, action.payload]
-    }
-    if ( action.type === COMPLETE_DAILY_BAD_HABIT ) {
-        let newHabitObject = [...state];
+    switch (action.type) {
+        case SAVE_NEW_BAD_HABIT:
+            return [...state, action.payload]
 
-        let today = getTodayDate();
+        case COMPLETE_DAILY_BAD_HABIT: {
+            let newHabitObject = [...state];
+            let today = getTodayDate();
 
+            newHabitObject[action.habitIndex].points += 172800 ;
+            newHabitObject[action.habitIndex].lastUpdated = today;
+            newHabitObject[action.habitIndex].weekCounter -= 1;
 
-        newHabitObject[action.habitIndex].points += 172800 ;
-
-        newHabitObject[action.habitIndex].lastUpdated = today;
-        newHabitObject[action.habitIndex].weekCounter -= 1;
-
-
-        return newHabitObject;
-    }
-
-    if (action.type === DECAY_HABITPOINTS_ON_START) {
-
-        let newHabitObject = [...state];
-        for (i = 0; i < newHabitObject.length; i++) {
-            let modifiedDecayPoints = Math.floor(action.decayPoints * newHabitObject[i].difficulity)
-            if ((newHabitObject[i].points - modifiedDecayPoints) <= 0 ) {
-                newHabitObject[i].points = 0;
-            } else {
-
-                newHabitObject[i].points -= modifiedDecayPoints;
-            }
+            return newHabitObject;
         }
-        return newHabitObject;
-
-    }
-
-    if (action.type === RESET_WEEKLY_COUNTER) {
-        let newHabitObject = [...state];
-        if(newHabitObject.length > 0) {
+        case DECAY_HABITPOINTS_ON_START: {
+            let newHabitObject = [...state];
             for (i = 0; i < newHabitObject.length; i++) {
-                newHabitObject[i].weekCounter = newHabitObject[i].weekCounterLimit;
+                let modifiedDecayPoints = Math.floor(action.decayPoints * newHabitObject[i].difficulity)
+                if ((newHabitObject[i].points - modifiedDecayPoints) <= 0 ) {
+                    newHabitObject[i].points = 0;
+                } else {
+
+                    newHabitObject[i].points -= modifiedDecayPoints;
+                }
             }
+            return newHabitObject;
         }
+        case RESET_WEEKLY_COUNTER: {
+            let newHabitObject = [...state];
+            if(newHabitObject.length > 0) {
+                for (i = 0; i < newHabitObject.length; i++) {
+                    newHabitObject[i].weekCounter = newHabitObject[i].weekCounterLimit;
+                }
+            }
 
-        return newHabitObject;
-
+            return newHabitObject;
+        }
+        default:
+            return state;
     }
-
-    return state;
 }
 
 const changeQuoteReducer = (state = {}, action) => {
-    if ( action.type === CHANGE_QUOTE ) {
-        return action.payload
+    switch(action.type) {
+        case CHANGE_QUOTE:
+            return action.payload
+        default:
+            return state;
     }
-
-    return state;
 }
 
 const updateStatusReducer = (state = {}, action) => {
-    if ( action.type === UPDATE_STATUS ) {
-        return {
-            // This will ensure that we decay about the same rate as seconds in a day
-            decayPoints: Math.floor((Date.now() - state.lastOnline) / 1000),
-            lastOnline: Date.now(),
-            nextWeeklyCounterReset: state.nextWeeklyCounterReset
-        }
-    }
+    switch(action.type) {
+        case UPDATE_STATUS:
+            return {
+                // This will ensure that we decay about the same rate as seconds in a day
+                decayPoints: Math.floor((Date.now() - state.lastOnline) / 1000),
+                lastOnline: Date.now(),
+                nextWeeklyCounterReset: state.nextWeeklyCounterReset
+            }
+        case RESET_WEEKLY_COUNTER:
+            return {...state, nextWeeklyCounterReset: getNextMonday()}
 
-    if (action.type === RESET_WEEKLY_COUNTER) {
-        return {...state, nextWeeklyCounterReset: getNextMonday()}
+        default:
+            return state;
     }
-
-    return state;
 }
 
 const reducer = combineReducers({
